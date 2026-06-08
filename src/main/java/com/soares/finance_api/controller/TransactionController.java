92d47f12febc10bcc5413c3e5348fb9b341b9381
diosoaresdev <diosoaresdev@gmail.com>
2026-06-08 18:27:41 -0300

    feat: cria TransactionController com endpoints de transacoes

--- src/main/java/com/soares/finance_api/controller/TransactionController.java
+++ src/main/java/com/soares/finance_api/controller/TransactionController.java
@@ -1,4 +1,42 @@
 package com.soares.finance_api.controller;
 
+
+import com.soares.finance_api.dto.TransactionRequest;
+import com.soares.finance_api.dto.TransactionResponse;
+import com.soares.finance_api.service.TransactionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transactions")
+@RequiredArgsConstructor
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @GetMapping
+    public ResponseEntity<List<TransactionResponse>> listAll() {
+        return ResponseEntity.ok(transactionService.listAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<TransactionResponse> create(@Valid @RequestBody TransactionRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.create(request));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<TransactionResponse> update(@PathVariable Long id, @RequestBody @Valid TransactionRequest request){
+        return ResponseEntity.ok(transactionService.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        transactionService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
